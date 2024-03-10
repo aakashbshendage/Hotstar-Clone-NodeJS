0153629f9e7683116d0b59fad3d6d771dc5b1e34
Aakash Balasaheb Shendage <144535729+aakashbshendage@users.noreply.github.com>
2024-03-10 22:56:44 +0530

    Update main.tf
    Updated line 15 (+1)

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "example" {
-  name               = "eks-cluster-cloud"
+  name               = "eks-cluster-cloud-1"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
