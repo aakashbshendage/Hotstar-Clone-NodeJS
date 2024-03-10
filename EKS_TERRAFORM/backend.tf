d98410d970b5b762e5ca7b0c8a48bee83466fa45
Aakash Balasaheb Shendage <144535729+aakashbshendage@users.noreply.github.com>
2024-03-10 20:54:00 +0530

    Update backend.tf
    Update bucket region us-east-1

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "aakashbshendage.ai" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = " us-east-1"
+    region = "us-east-1"
   }
 }
