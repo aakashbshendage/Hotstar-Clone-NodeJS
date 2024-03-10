491450da2225005d02c6f43c7c431ba84ad04cbc
Aakash Balasaheb Shendage <144535729+aakashbshendage@users.noreply.github.com>
2024-03-10 22:40:51 +0530

    Update backend.tf
    Updated S3 bucket name and region

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "aakashbshendage.ai" # Replace with your actual S3 bucket name
+    bucket = "aakashbshendage.hotstarclone" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
